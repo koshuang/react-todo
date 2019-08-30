387d9ffc4fbe86e0802ae8f142ab7342c77c549f
Kos <koshuang@gmail.com>
2019-08-31 04:57:08 +0800

    feat(entity): [TodoItem] added

---
+++ src/modules/todo-list/entities/TodoItem.ts
@@ -0,0 +1,9 @@
+export class TodoItem {
+  title: string;
+  completed: boolean;
+
+  constructor(title: string, completed: boolean) {
+    this.title = title;
+    this.completed = completed;
+  }
+}
