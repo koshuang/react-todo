dc31ce66a60525f09ee7784ac550a7a0f8690306
Kos <koshuang@gmail.com>
2019-08-31 08:12:18 +0800

    feat(todo-list/entity): [TodoItem] make completed default to false

--- src/modules/todo-list/entities/TodoItem.ts
+++ src/modules/todo-list/entities/TodoItem.ts
@@ -2,7 +2,7 @@ export class TodoItem {
   title: string;
   completed: boolean;
 
-  constructor(title: string, completed: boolean) {
+  constructor(title: string, completed: boolean = false) {
     this.title = title;
     this.completed = completed;
   }
