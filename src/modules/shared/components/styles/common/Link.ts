8446e82cece31f0ee92558e5ba71228d9bbff3a5
Kos <koshuang@gmail.com>
2019-08-31 08:04:32 +0800

    feat(shared/component): [Link] added

---
+++ src/modules/shared/components/styles/common/Link.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+/* Adapt the colors based on primary prop */
+export const Link = styled.a`
+  &:hover {
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
