d96252f3885d681508b9430b29191ed09ea0712c
Kos <koshuang@gmail.com>
2019-09-27 00:39:45 +0800

    refactor: move TodoDashboard to pages folder

--- src/modules/todo-list/components/index.ts
+++ src/modules/todo-list/components/index.ts
@@ -1,7 +1,6 @@
 export * from './styles';
 
 export * from './AddTodo';
-export * from './TodoDashboard';
 export * from './TodoForm';
 export * from './TodoHeader';
 export * from './TodoList';
--- src/modules/todo-list/index.ts
+++ src/modules/todo-list/index.ts
@@ -1,4 +1,5 @@
 export * from './components';
 export * from './entities';
 export * from './facades';
+export * from './pages';
 export * from './services';
